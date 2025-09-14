26abc5dbde6e6a44acb1a63b0a6592277a5efdee
Niharika Swain <swainniharika27@gmail.com>
2025-09-14 22:48:32 +0530

    updated

--- frontend/src/Components/FAQs.jsx
+++ frontend/src/Components/FAQs.jsx
@@ -14,15 +14,16 @@ const FAQs = () => {
   const [activeQuestion, setActiveQuestion] = useState(null);
 
   return (
-    <div className="w-screen h-screen bg-[linear-gradient(to_top,_#d5d4d0_0%,_#d5d4d0_1%,_#eeeeec_31%,_#efeeec_75%,_#e9e9e7_100%)] flex justify-center items-center">
-      <div className="w-[89%] m-auto max-w-[800px] bg-gray-300 p-8 rounded-lg shadow-md">
-        <h2 className="text-4xl mb-6 font-semibold">
-          Frequently asked Questions!
+    <div className="w-screen min-h-screen bg-[linear-gradient(to_top,_#d5d4d0_0%,_#d5d4d0_1%,_#eeeeec_31%,_#efeeec_75%,_#e9e9e7_100%)] flex justify-center items-start py-10">
+      <div className="w-[89%] m-auto max-w-[800px] bg-gray-300 p-6 rounded-lg shadow-md">
+        <h2 className="text-3xl md:text-4xl mb-6 font-semibold text-center md:text-left">
+          Frequently Asked Questions!
         </h2>
+
         {questions.map((q) => (
           <div key={q.id} className="mb-4 last:mb-0">
             <button
-              className={`w-full text-left text-2xl focus:outline-none p-4 rounded-lg shadow-md flex justify-between items-center transition-colors duration-300 
+              className={`w-full text-left text-lg md:text-2xl focus:outline-none p-4 rounded-lg shadow-md flex justify-between items-center transition-colors duration-300 
               ${
                 activeQuestion === q.id
                   ? "bg-red-700 text-white"
@@ -46,7 +47,8 @@ const FAQs = () => {
                   initial={{ opacity: 0, height: 0 }}
                   animate={{ opacity: 1, height: "auto" }}
                   exit={{ opacity: 0, height: 0 }}
-                  className="mt-2 text-gray-900 ml-4"
+                  transition={{ duration: 0.3 }}
+                  className="mt-2 text-gray-900 ml-4 overflow-hidden"
                 >
                   <p>{q.answer}</p>
                 </motion.div>
